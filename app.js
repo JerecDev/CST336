baea1bad8d402bebe55e4b1299e06cd86426cfaf
EC2 Default User <ec2-user@ip-172-31-24-77.us-west-2.compute.internal>
2020-02-03 06:41:59 +0000

    aboutpg

--- app.js
+++ app.js
@@ -22,6 +22,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+
+app.get('/about', (request, response) => {
+    response.render('about');
+});
+
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
