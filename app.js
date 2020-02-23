db295dfbd9ed9d3f6490d57555f4e6ec6cadbc3b
EC2 Default User <ec2-user@ip-172-31-24-77.us-west-2.compute.internal>
2020-02-23 05:55:27 +0000

    welcome

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 app.get('/welcome', (request, response) => {
-    response.render('views/welcome');
+    response.render('welcome.html');
 });
 
 
