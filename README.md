7683faf5f56121a796e1a7715169ca23d3a77eba
EC2 Default User <ec2-user@ip-172-31-24-77.us-west-2.compute.internal>
2020-03-08 21:08:32 +0000

    assign31

--- README.md
+++
@@ -1,7 +0,0 @@
-1a. 1. Inline 2. Internal 3. External
-1b. Margin is the space outside the elements border, padding is the space inside the border
-
-2a !== is a strict comparison which compares the value and type, != is a loose comparison which only checks the value
-2b. undefined, false
-
-3. [1, 2][1, 2]
---
+++ public/javascripts/java.js
@@ -0,0 +1,35 @@
+    genderLookup.addEventListener("click", function(){
+        
+    $(".nameBox").empty();
+    $(".genderBox").empty();
+
+
+  let name = $("#inputName").val(); 
+  
+  $.ajax({
+      method: "GET",
+      url: "https://www.behindthename.com/api/lookup.json",
+      dataType: "json",
+      data: {
+          "key": "za407011376",
+          "name" : name },
+      success: function(result){
+          console.log(result);
+          if(result[0].gender == "m"){
+              $("#nameBox").html("Name: " + result[0].name); 
+              $("#genderBox").html("Gender: Female");
+              $(".jumbotron").css("border", "solid pink 7px");
+              
+          } else if(result[0].gender == "f"){
+              $("#nameBox").html("Name: " + result[0].name); 
+              $("#genderBox").html("Gender: Male");
+              $(".jumbotron").css("border", "solid blue 7px");
+              
+          } else {
+              $("#nameBox").html("Name: " + result[0].name); 
+              $("#genderBox").html("Neutral Gender");
+              $(".jumbotron").css("border", "solid gray 7px");
+          }
+      }
+  });
+})
