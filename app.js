5d8d6e1a3eb496a25a8d6abcf698a525f9b12506
EC2 Default User <ec2-user@ip-172-31-24-77.us-west-2.compute.internal>
2020-03-15 23:34:52 +0000

    attempt

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const app = express();
+app.engine('html', require('ejs').renderFile);
+app.use(express.static("public"));
+app.use(express.static("css"));
+
+
+app.get("/", function(req, res){
+  res.render('index.html');
+});
+
+app.get("/mercury", function(req, res){
+  res.render('mercury.html');
+});
+
+app.get("/venus", function(req, res){
+  res.render('venus.html');
+});
+
+app.get("/earth", function(req, res){
+res.render('earth.html');
+});
+
+app.get("*", function(req, res){
+  res.render('error')
+});
+
+
+//listener
+app.listen(process.env.PORT, process.env.IP, function() {
+  console.log('Running Express Server...');
+});
+
+
+var server = app.listen(3000, function() {
+
+});
