48357bbdcd653973bfd3d9810421b9e9e26b7c2f
EC2 Default User <ec2-user@ip-172-31-24-77.us-west-2.compute.internal>
2020-03-21 22:12:23 +0000

    start

--- app.js
+++ app.js
@@ -1,45 +1,66 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-app.engine('html', require('ejs').renderFile);
-app.use(express.static("public"));
-app.use(express.static("css"));
+app.set("view engine", "ejs");
+app.use(express.static("public")); //folder for images, css, js
+
+const request = require('request');
+
+//routes
+app.get("/", async function(req, res){
+    
+ let parsedData = await getImages("otters");
+ 
+ console.dir("parsedData: " + parsedData); //displays content of the object
+    
+ res.render("index", {"image":parsedData.hits[0].largeImageURL});
+            
+}); //root route
 
 
-app.get("/", function(req, res){
-  res.render('index.html');
-});
+app.get("/results", async function(req, res){
+    
+    //console.dir(req);
+    let keyword = req.query.keyword; //gets the value that the user typed in the form using the GET method
+    
+    let parsedData = await getImages(keyword);
 
-app.get("/mercury", function(req, res){
-  res.render('mercury.html');
-});
-
-app.get("/venus", function(req, res){
-  res.render('venus.html');
-});
-
-app.get("/earth", function(req, res){
-res.render('earth.html');
-});
-
-app.get("/uranus", function(req, res){
-res.render('uranus.html');
-});
-
-app.get("/jupiter", function(req, res){
-res.render('jupiter.html');
-});
-
-app.get("*", function(req, res){
-  res.render('error')
-});
+    res.render("results", {"images":parsedData});
+    
+});//results route
 
 
-//listener
-app.listen(process.env.PORT, process.env.IP, function() {
-  console.log('Running Express Server...');
-});
+//Returns all data from the Pixabay API as JSON format
+function getImages(keyword){
+    
+    
+    return new Promise( function(resolve, reject){
+        request('https://pixabay.com/api/?key=5589438-47a0bca778bf23fc2e8c5bf3e&q='+keyword,
+                 function (error, response, body) {
+    
+            if (!error && response.statusCode == 200  ) { //no issues in the request
+                
+                 let parsedData = JSON.parse(body); //converts string to JSON
+                 
+                 resolve(parsedData);
+                
+                //let randomIndex = Math.floor(Math.random() * parsedData.hits.length);
+                //res.send(`<img src='${parsedData.hits[randomIndex].largeImageURL}'>`);
+                //res.render("index", {"image":parsedData.hits[randomIndex].largeImageURL});
+                
+            } else {
+                reject(error);
+                console.log(response.statusCode);
+                console.log(error);
+            }
+    
+          });//request
+   
+    });
+    
+}
 
 
-var server = app.listen(3000, function() {
-
-});
+//starting server
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("Express server is running...");
+})
