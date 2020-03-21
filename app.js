0f91c091faa103be28aa95ef5d2070a8f76a2b20
EC2 Default User <ec2-user@ip-172-31-24-77.us-west-2.compute.internal>
2020-03-21 23:49:36 +0000

    randumbs

--- app.js
+++ app.js
@@ -16,10 +16,7 @@ app.get("/", async function(req, res){
  
  console.dir("parsedData: " + parsedData); //displays content of the object
     
- res.render("index", {"image":parsedData.hits[0].largeImageURL});
- res.render("index", {"image2":parsedData.hits[0].largeImageURL});
- res.render("index", {"image3":parsedData.hits[0].largeImageURL});
- res.render("index", {"image4":parsedData.hits[0].largeImageURL});
+ res.render("index", {"image":parsedData.hits.largeImageURL});
             
 }); //root route
 
