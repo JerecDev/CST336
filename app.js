7b201a69e5dcc341b08550bb42931efc07e9d94e
EC2 Default User <ec2-user@ip-172-31-24-77.us-west-2.compute.internal>
2020-04-05 23:42:52 +0000

    newpages

--- app.js
+++ app.js
@@ -26,22 +26,6 @@ app.get("/java", function (req, res){
     res.render("java");
 });
 
-
-
-
-app.get("/results", function(req, res){
-    var query = req.query.search; //gets the value that the user typed in the form using the GET method
-	var url = 'https://openlibrary.org/api/books?bibkeys=ISBN:' + query + '&format=json&jscmd=data';
-	request(url, function(error, response, dataStream){
-		if (!error && response.statusCode == 200){
-			var data = JSON.parse(dataStream);
-			res.render("results", {data:data, query:query});
-		}
-	});
-});//results route
-
-
-
 //starting server
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Express server is running...");
