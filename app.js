b6c7ca8e65030086b7e4d7d26dcdd5995a11efbd
EC2 Default User <ec2-user@ip-172-31-24-77.us-west-2.compute.internal>
2020-04-06 00:49:45 +0000

    newpagesss

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ app.use(express.static("css"));
 app.use(express.static("public"));
 
 //routes
-app.get("/", async function (req, res){
-    res.render("index");
+app.get("/", function (req, res){
+    res.render("c++");
 }); //root route
 
 
