41605a21a7a6796f835122c4a26872eba88d672c
EC2 Default User <ec2-user@ip-172-31-24-77.us-west-2.compute.internal>
2020-04-19 19:29:29 +0000

    lab9init

--- app.js
+++ app.js
@@ -1,32 +1,63 @@
-const express = require("express");
-const app = express();
-app.set("view engine", "ejs");
+/* Require external APIs and start our application instance */
+var express = require('express');
+var mysql = require('mysql');
+var app = express();
 
-app.use(express.static("css"));
-app.use(express.static("public"));
+/* Configure our server to read public folder and ejs files */
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
 
-//routes
-app.get("/", function (req, res){
-    res.render("index.ejs");
-}); //root route
+/* Configure MySQL DBMS */
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'admin',
+    password: 'password',
+    database: 'quotes_db'
+});
+connection.connect();
 
-app.get("/index", function (req, res){
-    res.render("index.ejs");
+/* The handler for the DEFAULT route */
+app.get('/', function(req, res){
+    res.render('home');
 });
 
-app.get("/c", function (req, res){
-    res.render("c.ejs");
+/* The handler for the /author route */
+app.get('/author', function(req, res){
+    var stmt = 'select * from l9_author where firstName=\'' 
+                + req.query.firstname + '\' and lastName=\'' 
+                + req.query.lastname + '\';'
+	connection.query(stmt, function(error, found){
+	    var author = null;
+	    if(error) throw error;
+	    if(found.length){
+	        author = found[0];
+	        // Convert the Date type into the String type
+	        author.dob = author.dob.toString().split(' ').slice(0,4).join(' ');
+	        author.dod = author.dod.toString().split(' ').slice(0,4).join(' ');
+	    }
+	    res.render('author', {author: author});
+	});
 });
 
-app.get("/pascal", function (req, res){
-    res.render("pascal.ejs");
+/* The handler for the /author/name/id route */
+app.get('/author/:aid', function(req, res){
+    var stmt = 'select quote, firstName, lastName ' +
+               'from l9_quotes, l9_author ' +
+               'where l9_quotes.authorId=l9_author.authorId ' + 
+               'and l9_quotes.authorId=' + req.params.aid + ';'
+    connection.query(stmt, function(error, results){
+        if(error) throw error;
+        var name = results[0].firstName + ' ' + results[0].lastName;
+        res.render('quotes', {name: name, quotes: results});      
+    });
 });
 
-app.get("/java", function (req, res){
-    res.render("java.ejs");
+/* The handler for undefined routes */
+app.get('*', function(req, res){
+   res.render('error'); 
 });
 
-//starting server
-app.listen(process.env.PORT, process.env.IP, function(){
-    console.log("Express server is running...");
-});
+/* Start the application server */
+app.listen(process.env.PORT || 3000, function(){
+    console.log('Server has been started');
+})
